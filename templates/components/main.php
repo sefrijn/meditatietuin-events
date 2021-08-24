fb6b6d2b1311502228fbd5a94546bf93be14d2e5
How About Yes <sefrijn@gmail.com>
2021-08-24 20:30:42 +0200

    Visual updates

--- templates/components/main.php
+++ templates/components/main.php
@@ -1,5 +1,5 @@
-<main class="pb-6 md:order-2 order-1 w-full xl:w-4/5 lg:w-3/4 md:w-2/3 px-4 md:px-12">
-	<div class="max-w-prose">
+<main class="pb-16 md:order-2 order-1 w-full xl:w-4/5 lg:w-3/4 md:w-2/3 px-4 md:pl-12 md:pr-2">
+	<div class="max-w-screen-md">
 		<?php the_content(); ?>
 	</div>
 </main>
